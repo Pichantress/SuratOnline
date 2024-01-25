78dff7574a998ee4a38dcf1d0a25fb583bf87a08
Wahyu Arif Maulana <116780473+Pichantress@users.noreply.github.com>
2024-01-25 21:42:06 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Surat Online Bulakan</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -24,5 +24,8 @@
                 {{ $slot }}
             </main>
         </div>
+        <footer class="text-center text-sm text-gray-500 bg-gray-200 py-2">
+            &copy; {{ date('Y') }} Surat Online Desa Bulakan created By BNW KKN UNDIP.
+        </footer>
     </body>
 </html>
