58c10bbe0207bd034adf858c3b2060c848ae47f8
Wahyu Arif Maulana <116780473+Pichantress@users.noreply.github.com>
2024-01-25 21:42:52 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>Surat Online Bulakan</title>
 
+        <link rel="icon" type="image/png" href="{{ asset('img/LOGO-WEB.png') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
