e9c1de5b4093c85009e2461ed68a895845f87704
Wahyu Arif Maulana <116780473+Pichantress@users.noreply.github.com>
2024-01-25 21:44:06 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Surat Online Bulakan</title>
+
+        <link rel="icon" type="image/png" href="{{ asset('img/LOGO-WEB.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
