9ea027ebf85d39dbb8e7e72717fae7ab52e91050
Pichantress <wahyuarif582@gmail.com>
2024-01-25 21:18:35 +0700

    SuratOnlineBulakan

---
+++ app/Http/Controllers/UserLinksController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserLinksController extends Controller
+{
+    public function __invoke(User $user)
+    {
+        $user = $user->load('links');
+
+        return view('user.links', compact('user'));
+    }
+}
+ 
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+    <image href="{{ asset('img/LOGO-WEB.png') }}" width="316" height="316" />
+</svg>
